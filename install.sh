181dc11dfee91b6b86ba660561d8886e7e4aa915
Ali Avani <90338857+ali-avani@users.noreply.github.com>
2023-08-08 19:44:21 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -247,6 +247,7 @@ install_nginx_webmin() {
     echo_run "gcf $PROJECT_CONFIGS/webmin/webmin.conf > /etc/nginx/sites-available/webmin.conf"
     echo_run "ln -s /etc/nginx/sites-available/webmin.conf /etc/nginx/sites-enabled/"
     echo_run "certbot --nginx -d $DOMAIN -d webmin.$DOMAIN --noninteractive --expand"
+    echo "URL: webmin.$DOMAIN"
 }
 
 install_nginx_usermin() {
@@ -259,6 +260,7 @@ install_nginx_usermin() {
     echo_run "gcf $PROJECT_CONFIGS/usermin/usermin.conf > /etc/nginx/sites-available/usermin.conf"
     echo_run "ln -s /etc/nginx/sites-available/usermin.conf /etc/nginx/sites-enabled/"
     echo_run "certbot --nginx -d $DOMAIN -d user.$DOMAIN --noninteractive --expand"
+    echo "URL: user.$DOMAIN"
 }
 
 install_namizun() {
@@ -327,6 +329,8 @@ ACTIONS=(
     setup_firewall
     build_mtproxy
     run_mtproxy
+    backup_pam_users
+    restore_pam_users
 )
 
 # READ ACTIONS
