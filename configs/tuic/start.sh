4275750272ab9651b553dfa737848ff9281ecfb1
Ali Avani <ali.avani@outlook.com>
2023-09-08 03:12:31 +0330

    Add tuic

--- configs/tuic/start.sh
+++ configs/tuic/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls /root/config-org.json | entr -nr bash -c "sleep 1 && cat /root/config-org.json && cp /root/config-org.json /root/config.json && /root/tuic -c /root/config.json"
+ls /root/configs/config-org.json | entr -nr bash -c "sleep 4 && cat /root/configs/config-org.json && cp /root/configs/config-org.json /root/config.json && /root/tuic -c /root/config.json"
