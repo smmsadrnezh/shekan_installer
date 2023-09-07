acb05480282a3e47f839b4b1cbbd4667a251881e
Ali Avani <ali.avani@outlook.com>
2023-09-08 02:58:22 +0330

    Add tuic

--- configs/tuic/start.sh
+++ configs/tuic/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls /root/config.json | entr -nr bash -c "sleep 1 && /root/tuic -c /root/config.json && echo 'users updated'"
+ls /root/config.json | entr -nr bash -c "sleep 1 && cat /root/config.json && /root/tuic -c /root/config.json && echo 'users updated'"
